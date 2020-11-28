b7ad465934bfa1979d28efc40f15aadf3568a4ab
hglee93 <jeuslhg@gmail.com>
2020-11-28 22:11:26 +0900

    Add StockItemDetail, StockPredictionView
    - StockItemDetail 페이지 추가 : 테스트용도로 추가되었고, 추후 페이지 추가될 경우 삭제
    - StockPredictionView 추가 : 예측 결과 Component 추가, 클릭 이벤트 추가

---
+++ src/components/StockPredictionView.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './StockPredictionView.css';
+
+class StockPrectionView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            IsOpen: false,
+            IsGraphOpen: false,
+        }
+    }
+
+    handleClick() {
+        this.setState({
+            IsOpen: !this.state.IsOpen
+        });
+
+        if(!this.state.IsOpen) {
+            var foreground = document.getElementsByClassName('foreground')[0];
+            foreground.style.display = 'none';
+
+            var background = document.getElementsByClassName('background')[0];
+            background.style.filter = "blur(0)";
+        }
+    }
+
+    handleGraphOpen() {
+
+        this.setState({
+            IsGraphOpen: !this.state.IsGraphOpen
+        });
+
+        var graph = document.getElementsByClassName('graph')[0];
+
+        if(!this.state.IsGraphOpen) {
+            graph.style.display = 'block';
+        }
+        else {
+            graph.style.display = 'none';
+        }
+    }
+
+    render() {
+
+        let graphButtonText;
+        if(this.state.IsGraphOpen) {
+            graphButtonText = '접기';
+        }
+        else {
+            graphButtonText = '펼치기';
+        }
+
+        return (
+            <div className='wrapper'>
+                <div className='foreground'>
+                    <div className='foreground notice'>
+                        <p><b>30포인트를 지불하고 주가 예측을 확인하세요!</b></p>
+                        <button type='button' onClick={() => this.handleClick()}>포인트 지불</button>
+                    </div>
+                </div>
+                <div className='background'>
+                    <p><b>내일 주가 예측 59000원 (▼700  -0.33%)입니다.</b></p>
+        <button type='button' onClick={() => this.handleGraphOpen()}>{graphButtonText}</button>
+                    <div className='graph'>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StockPrectionView;
---
+++ src/containers/StockItemDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StockPrectionView from '../components/StockPredictionView';
+
+function StockItemDetails() {
+    return (
+        <div className="content_search">
+            <div>StockItemDetails</div>
+            <StockPrectionView></StockPrectionView>
+        </div>
+    )
+}
+
+export default StockItemDetails;
