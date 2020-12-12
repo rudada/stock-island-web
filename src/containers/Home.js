653a3189f49030e1200884489b8fd2403b07a9c7
seazero <withbb96@gmail.com>
2020-12-12 18:27:39 +0900

    연결 테스트

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import HomeSearchbar from '../components/HomeSearchbar'
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
+import Cloud from './Cloud.js'
+
+import ModalPage from './Modal.js'
+
 
 function Home() {
     return (
-        <div className="content_home">
-            <HomeSearchbar></HomeSearchbar>
-            <div>Home</div>
-        </div>
+
+
+<MDBContainer className="content_home">
+
+<MDBRow>
+<HomeSearchbar></HomeSearchbar>
+{/* <ModalPage /> */}
+</MDBRow>
+    <MDBRow middle >
+    <Cloud />
+    
+    </MDBRow>
+    <MDBRow>
+                 <MDBCol size="4" >주간 기업평가 목록<br /><br /><br /><br /><br /><br /><br /><br /><br /></MDBCol>
+                 <MDBCol size="4" >주가 예측 상승률 / 하락률 순위<br /><br /><br /><br /><br /><br /><br /><br /><br /></MDBCol>
+                 <MDBCol size="4" >게시글 조회수 순위<br /><br /><br /><br /><br /><br /><br /><br /><br /></MDBCol>
+           </MDBRow>
+           
+        </MDBContainer>
+        
     )
 }
 
--- src/containers/Weekly.js
+++ src/containers/Weekly.js
@@ -1,12 +1,97 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import Searchbar from '../components/Searchbar'
+import DatePicker, { registerLocale } from "react-datepicker";
+import getYear from "date-fns/getYear";
+import getMonth from "date-fns/getMonth";
+import ko from 'date-fns/locale/ko';
+registerLocale("ko", ko);
+
 
 
 function Weekly() {
+const [currentDate, setCurrentDate] = useState();
+const calendar = useRef(null);
+
+const cancelDatePicker = () => {
+  setStartDate(currentDate);
+  calendar.current.setOpen(false);
+};
+
+const openDatePicker = () => {
+  calendar.current.setOpen(true);
+};
+
+const closeDatePicker = () => {
+  setCurrentDate(startDate);
+  calendar.current.setOpen(false);
+};
+
+<DatePicker
+                withPortal
+                className="date date-record"
+                locale="ko"
+                selected={startDate}
+                minDate={minDate}
+                maxDate={maxDate}
+                dateFormat="yyyy.MM.dd(eee)"
+                useWeekdaysShort={true}
+                shouldCloseOnSelect={false}
+                useWeekdaysShort={true}
+                excludeDates={excludeDates}
+                ref={calendar}
+                onInputClick={() => openDatePicker()}
+                onChange={(date, event) => datePickHandler(date, event)}
+                renderCustomHeader={({
+                    date,
+                    prevMonthButtonDisabled,
+                    nextMonthButtonDisabled,
+                    decreaseMonth,
+                    increaseMonth,
+                }) => (
+                    <div
+                    style={{
+                        margin: 10,
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                    >
+                    <div
+                        className="btn_month btn_month-prev"
+                        onClick={decreaseMonth}
+                        disabled={prevMonthButtonDisabled}
+                    >
+                        <img src="/static/images/arrow-black-left.png" />
+                    </div>
+                    <div className="month-day">
+                        {getYear(date)}.{months[getMonth(date)]}
+                    </div>
+
+                    <div
+                        className="btn_month btn_month-next"
+                        onClick={increaseMonth}
+                        disabled={nextMonthButtonDisabled}
+                    >
+                        <img src="/static/images/arrow-black-right.png" />
+                    </div>
+                    </div>
+                )}
+                >
+                <div className="button-container">
+                    <div className="btn_ctrl btn_ctrl-cancel" onClick={cancelDatePicker}>
+                    {" "}
+                    취소
+                    </div>
+                    <div className="btn_ctrl btn_ctrl-confirm" onClick={closeDatePicker}>
+                    선택
+                    </div>
+                </div>
+            </DatePicker>
     return (
+        
         <div  className="content_weekly">
             <Searchbar></Searchbar>
             Weekly
+            
         </div>
     );
 };
