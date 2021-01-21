1c4ae60986739693ee6dee4390c2cb729c9dcfdf
rudada <37798996+rudada@users.noreply.github.com>
2021-01-21 23:00:00 +0900

    react-bootstrap  제거

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+    MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
+    } from "mdbreact";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css'
 
@@ -13,9 +19,14 @@ import Search from './containers/Search';
 import StockItemDetails from './containers/StockItemDetails';
 
 function App() {
+    const [isOpen, setOpen] = useState(false);
+    const toggleCollapse = () => {
+        console.log('hi')
+    }
+
     return (
         <Router>
-            <Navbar bg="dark" variant="dark" scrolling="true" fixed="top">
+            {/* <Navbar bg="dark" variant="dark" scrolling="true" fixed="top">
                 <Navbar.Brand href="/">StockIsland</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="/board">게시판</Nav.Link>
@@ -24,8 +35,48 @@ function App() {
                     <Nav.Link href="/login">로그인</Nav.Link>
                     <Nav.Link href="/details">상세페이지(테스트)</Nav.Link>
                 </Nav>
-            </Navbar>
-            <br/><br/><br/>
+            </Navbar> */}
+            {/* <Breadcrumbs aria-label="breadcrumb">
+                <Link color="inherit" href="/">StockIsalnd</Link>
+                <Link color="inherit" href="/board">게시판</Link>
+                <Link color="inherit" href="/weekly">주간평가</Link>
+                <Link color="inherit" href="/mypage">마이페이지</Link>
+                <Link color="inherit" href="/login">로그인</Link>
+                <Link color="inherit" href="/details">상세페이지(테스트)</Link>
+            </Breadcrumbs>
+            <br /><br /><br /> */}
+
+<MDBNavbar color="indigo" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">Navbar</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={isOpen} navbar>
+          <MDBNavbarNav left>
+            <MDBNavItem active>
+              {/* <MDBNavLink href="/">Home</MDBNavLink> */}
+              {/* <button href="/mypage">hi</button> */}
+            </MDBNavItem>
+            <MDBNavItem>
+              {/* <MDBNavLink to="#!">Features</MDBNavLink> */}
+            </MDBNavItem>
+            <MDBNavItem>
+              {/* <MDBNavLink to="#!">Pricing</MDBNavLink> */}
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              {/* <MDBFormInline waves>
+                <div className="md-form my-0">
+                  <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                </div>
+              </MDBFormInline> */}
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+
+
             <div className="wrapper">
                 <Route exact path="/" component={Home} />
                 <Route exact path="/board" component={Board} />
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    // <React.StrictMode>
+        <App />,
+    // </React.StrictMode>,
     document.getElementById('root')
 );
