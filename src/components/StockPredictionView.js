56f375109e5aed07ebda35a46996851eaa717c70
hglee93 <jeuslhg@gmail.com>
2021-02-13 00:36:50 +0900

    Modify StockPredictionView 이미지

--- src/components/StockPredictionView.js
+++ src/components/StockPredictionView.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './StockPredictionView.css';
 
+import banner from '../img/init_banner.png';
+
 class StockPrectionView extends React.Component {
 
     constructor(props) {
@@ -16,7 +18,7 @@ class StockPrectionView extends React.Component {
             IsOpen: !this.state.IsOpen
         });
 
-        if(!this.state.IsOpen) {
+        if(this.state.IsOpen == true) {
             var foreground = document.getElementsByClassName('foreground')[0];
             foreground.style.display = 'none';
 
@@ -51,22 +53,33 @@ class StockPrectionView extends React.Component {
             graphButtonText = '펼치기';
         }
 
-        return (
-            <div className='stock-prediction-view'>
-                <div className='foreground'>
-                    <div className='notice'>
-                        <p><b>30포인트를 지불하고 주가 예측을 확인하세요!</b></p>
-                        <button type='button' onClick={() => this.handleClick()}>포인트 지불</button>
+        if(this.state.IsOpen == true) {
+            return (
+                <div className='stock-prediction-view'>
+                    <div className='background'>
+                        <p><b>내일 주가 예측 59000원 (▼700  -0.33%)입니다.</b></p>
+                        <button type='button' onClick={() => this.handleGraphOpen()}>{graphButtonText}</button>
+                        <div className='graph'>
+                        </div>
                     </div>
                 </div>
-                <div className='background'>
-                    <p><b>내일 주가 예측 59000원 (▼700  -0.33%)입니다.</b></p>
-                    <button type='button' onClick={() => this.handleGraphOpen()}>{graphButtonText}</button>
-                    <div className='graph'>
+            );
+        }
+        else {
+            return (
+                <div className='stock-prediction-view'>
+                    <div className='foreground'>
+                        <div className='notice'>
+                            <p><b>30포인트를 지불하고 주가 예측을 확인하세요!</b></p>
+                            <button type='button' onClick={() => this.handleClick()}>포인트 지불</button>
+                        </div>
+                    </div>
+                    <div className='background-image'>
+                        <img src={banner} width='500' height='100' ></img>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
